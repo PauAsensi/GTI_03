2c3e3a89cf4876856bdbe9d80d1dd713beeb99cd
Irene Medina García <44927706+irenemg8@users.noreply.github.com>
2023-05-02 18:42:58 +0200

    Presupuesto js

---
+++ js/filtrosPresupuesto.js
@@ -0,0 +1,37 @@
+/*---- Elegir provincia ----*/
+/*
+$(document).ready(function() {
+    // Detectar cuando se escribe en el campo de texto
+    $('#provincia-autocomplete').on('input', function() {
+        let query = $(this).val().toLowerCase();
+        let options = $('#provincia option');
+
+        // Ocultar todas las opciones
+        options.hide();
+
+        // Mostrar solo las opciones que coinciden con la consulta
+        options.filter(function() {
+            return $(this).text().toLowerCase().indexOf(query) !== -1;
+        }).show();
+
+        // Mostrar el campo de selección de provincia si hay opciones disponibles
+        $('#provincia').toggle(options.filter(':visible').length > 0);
+    });
+});
+
+*/
+
+/*--------- Mostrar el popup ------*/
+function mostrarPopup() {
+    const popup = document.getElementById("popup");
+    console.log('entra');
+    popup.style.display = "block";
+    setTimeout(cerrarPopup, 6000);
+}
+
+function cerrarPopup() {
+    const popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+
+
