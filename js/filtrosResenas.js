a596de1678d71fddea6f341ff904946a87478b42
Irene Medina García <44927706+irenemg8@users.noreply.github.com>
2023-05-02 18:40:12 +0200

    reseñas

---
+++ js/filtrosResenas.js
@@ -0,0 +1,61 @@
+// Obtener los botones de ordenamiento
+const btnSortDateDescending = document.querySelector('#btn-sort-date-descending');
+const btnSortDateAscending = document.querySelector('#btn-sort-date-ascending');
+const btnSortRatingDescending = document.querySelector('#btn-sort-rating-descending');
+const btnSortRatingAscending = document.querySelector('#btn-sort-rating-ascending');
+
+// Obtener el contenedor de las reseñas
+const reviewsContainer = document.querySelector('.reviews-container');
+
+// Obtener todas las reseñas
+const reviews = Array.from(document.querySelectorAll('.review'));
+
+// Función para ordenar las reseñas por fecha (más reciente primero)
+function sortReviewsByDateDescending() {
+    reviews.sort((a, b) => {
+        const dateA = new Date(a.getAttribute('data-date'));
+        const dateB = new Date(b.getAttribute('data-date'));
+        return dateB - dateA;
+    });
+
+    reviews.forEach(review => reviewsContainer.appendChild(review));
+}
+
+// Función para ordenar las reseñas por fecha (más antiguo primero)
+function sortReviewsByDateAscending() {
+    reviews.sort((a, b) => {
+        const dateA = new Date(a.getAttribute('data-date'));
+        const dateB = new Date(b.getAttribute('data-date'));
+        return dateA - dateB;
+    });
+
+    reviews.forEach(review => reviewsContainer.appendChild(review));
+}
+
+// Función para ordenar las reseñas por valoración (más altas primero)
+function sortReviewsByRatingDescending() {
+    reviews.sort((a, b) => {
+        const ratingA = parseInt(a.getAttribute('data-rating'));
+        const ratingB = parseInt(b.getAttribute('data-rating'));
+        return ratingB - ratingA;
+    });
+
+    reviews.forEach(review => reviewsContainer.appendChild(review));
+}
+
+// Función para ordenar las reseñas por valoración (más bajas primero)
+function sortReviewsByRatingAscending() {
+    reviews.sort((a, b) => {
+        const ratingA = parseInt(a.getAttribute('data-rating'));
+        const ratingB = parseInt(b.getAttribute('data-rating'));
+        return ratingA - ratingB;
+    });
+
+    reviews.forEach(review => reviewsContainer.appendChild(review));
+}
+
+// Asignar los eventos click a los botones de ordenamiento
+btnSortDateDescending.addEventListener('click', sortReviewsByDateDescending);
+btnSortDateAscending.addEventListener('click', sortReviewsByDateAscending);
+btnSortRatingDescending.addEventListener('click', sortReviewsByRatingDescending);
+btnSortRatingAscending.addEventListener('click', sortReviewsByRatingAscending);
