d24f803bbd6be2bb7138a19b7aaa8d3d74c1e980
eleenaruizzz <elenaruizblanca@gmail.com>
2023-05-02 11:40:02 +0200

    faq y mis huertos

---
+++ src/js/login.js
@@ -0,0 +1,31 @@
+
+const input_nombre=document.getElmenetById('username');
+const input_passwd=document.getElmenetById('passwd');
+const btn_enviar=document.getElmenetById('enviar');
+
+btn_enviar.addEventListener('click',()=>{
+    if(input_nombre.value=='user'){
+        if(input_passwd=='user'){
+//El login esta correctamente
+        }else{
+//La contraseña esta mal
+        }
+    }else{
+//El usuario esta mal
+    }
+});
+const input_nombre=document.getElmenetById('username');
+const input_passwd=document.getElmenetById('passwd');
+const btn_enviar=document.getElmenetById('enviar');
+
+btn_enviar.addEventListener('click',()=>{
+    if(input_nombre.value=='user'){
+        if(input_passwd=='user'){
+//El login esta correctamente
+        }else{
+//La contraseña esta mal
+        }
+    }else{
+//El usuario esta mal
+    }
+});
